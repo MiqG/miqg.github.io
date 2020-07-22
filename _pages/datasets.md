456d98f7df2f59c3e1628ffdd09c71c095545b0f
MiqG <miquelangladagirotto@gmail.com>
2020-07-22 11:02:47 +0200

    updated posts and images

---
+++ _pages/datasets.md
@@ -0,0 +1,10 @@
+---
+layout: archive
+title: "DATASETS"
+permalink: "/datasets/"
+author_profile: true
+header:
+  image: "/images/lab.JPG"
+---
+
+Find the links to my favourite ready-to-use datasets.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,7 @@
 var store = [{
-        "title": "This is another test",
-        "excerpt":"R Markdown This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see http://rmarkdown.rstudio.com. When you click the Knit button a document will be generated that includes both content as well as the...","categories": [],
-        "tags": ["Data Resources"],
-        "url": "http://localhost:4000/testtt/",
-        "teaser": null
-      },{
-        "title": "this is a title",
-        "excerpt":"bla lba  import numpy as np   \\[x + y\\] ","categories": [],
-        "tags": ["Bioinformatics pipelines"],
-        "url": "http://localhost:4000/test/",
-        "teaser": null
-      },{
-        "title": "How to - Survival Analysis",
-        "excerpt":"hy import numpy as np import pandas as pd import matplotlib.pyplot as plt from lifelines.statistics import logrank_test from lifelines.utils import concordance_index from lifelines import CoxPHFitter # generate (un)censored time data np.random.seed(123) n = 100 time = np.random.randint(100, size = n) events = np.random.choice([0,1], size = n) color_keys = ['blue','orange'] #...","categories": [],
+        "title": "Dissecting 28 years of European pharmaceutical development",
+        "excerpt":"An exploration of data scraped from the European Medicines Agency (EMA) Since the discovery of antibiotics, the availability of new drugs has substantially improved our lives. Drug development requires high investments that come with high uncertainty because we need to logically prove the safety and efficacy of new drug candidates...","categories": [],
         "tags": ["Data Analysis"],
-        "url": "http://localhost:4000/howto-survival_analysis/",
+        "url": "http://localhost:4000/eda_ema/",
         "teaser": null
       }]
